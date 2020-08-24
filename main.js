898e05d7e33436246477f081cd78ffecb70d4794
didialfansyah <didialfansyah@gmail.com>
2020-08-24 22:20:23 +0700

    await update
    go

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let gData = []
 const getGiphy = async() => {
     const response = await fetch("https://api.giphy.com/v1/gifs/search?q=kirito&api_key=AhUN5gDNtxIy8xx84zR8BueNRu6M1b0e&limit=10")
     const data = await response.json();
-    gData = data.data;
+    gData = await data.data;
     await showData(gData);
     console.log(data);
 }
